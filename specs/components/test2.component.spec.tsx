fcf01ada4addbbd5f93e49acdd521f95f4fdf2d1
Igor Chulinda <ichulinda@dataart.com>
2016-11-08 15:17:24 +0100

    Fix for coverage problem

--- specs/components/test2.component.spec.tsx
+++ specs/components/test2.component.spec.tsx
@@ -11,7 +11,7 @@ describe('TestComponent', () => {
       const wrapper = shallow(<TestComponent />);
 
       expect(wrapper.html()).toEqual('<div></div>');
-      setTimeout(() => done, jasmine.DEFAULT_TIMEOUT_INTERVAL);
+      setTimeout(done, jasmine.DEFAULT_TIMEOUT_INTERVAL);
     });
 
   });
